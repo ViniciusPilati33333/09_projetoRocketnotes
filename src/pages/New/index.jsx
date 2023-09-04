1b75f2a37499a574b71745d334e15fe7380143ea
Vinicius <vini1718a@gmail.com>
2023-09-04 19:18:54 -0300

    add links in note

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Header } from '../../components/header';
 import { Input } from '../../components/input';
@@ -9,8 +10,17 @@ import { Button } from '../../components/button'
 
 
 import { Container, Form } from './style';
+import { FiTarget } from "react-icons/fi";
 
 export function New() {
+    const [links, setLinks] = useState([]);
+    const [newLink, setNewLink] = useState("");
+
+    function handleAddLink() {
+        setLinks(prevState => [...prevState, newLink]);
+        setNewLink("");
+    }
+
     return(
         <Container>
             <Header />
@@ -25,8 +35,24 @@ export function New() {
             <Textarea placeholder='Observações'/> 
 
             <Section title='Links úteis'>
-                <NoteItem value='https://github.com/ViniciusPilati33333' />
-                <NoteItem isNew placeholder="Novo Link"/>
+
+                {
+                    links.map((link, index) => ( 
+                <NoteItem 
+                    key={String(index)}
+                    value={link}
+                    onClick={() => { }}
+                />
+                    ))
+                }
+
+                <NoteItem 
+                isNew
+                placeholder="Novo Link"
+                value={newLink}
+                onChange={e => setNewLink(e.target.value)}
+                onClick={handleAddLink}
+                />
             </Section> 
 
             <Section title="Marcadores">
